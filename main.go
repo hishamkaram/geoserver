762818faff15e65e157082f377d0497064bf4050
hisham waleed karam <hisham.karam@cartologic.com>
2018-03-27 02:51:57 +0200

    add http server

--- main.go
+++ main.go
@@ -3,31 +3,83 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"html/template"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 var geoserverURL = "http://localhost:8080/geoserver/"
 var workspace = "golang"
+var uploadedPath = "./uploaded/"
 
+//GeoServer is configuration struct
+type GeoServer struct {
+	WorkspaceName string
+	ServerUrl     string
+	Username      string
+	Password      string
+}
+
+var geoserver = GeoServer{WorkspaceName: workspace, ServerUrl: geoserverURL, Username: "admin", Password: "geoserver"}
+
+func handleUploaded(file *bytes.Buffer, filename string) string {
+	_ = os.Mkdir(uploadedPath, 0700)
+	filepath := uploadedPath + filename
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	return filepath
+}
+func index(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		file, handler, err := r.FormFile("fileupload")
+		defer file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, file); err != nil {
+			panic(err)
+		}
+		uploadedPath := handleUploaded(buf, handler.Filename)
+		uploadShapeFile(uploadedPath)
+	}
+	tmplt := template.New("home.html")
+	tmplt, _ = tmplt.ParseFiles("templates/home.html")
+
+	tmplt.Execute(w, geoserver)
+}
 func main() {
-	fmt.Print("Enter Zip File Path: ")
-	var filePATH string
-	fmt.Scanln(&filePATH)
-	uploadShapeFile(filePATH)
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/static/").Handler(s)
+	http.Handle("/", r)
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
+	}
 }
 func createWorkspace(workspaceName string) (bool, error) {
 	var xml = fmt.Sprintf("<workspace><name>%s</name></workspace>", workspaceName)
-	var targetURL = fmt.Sprintf("%srest/workspaces", geoserverURL)
+	var targetURL = fmt.Sprintf("%srest/workspaces", geoserver.ServerUrl)
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", targetURL, bytes.NewBuffer([]byte(xml)))
 	if err != nil {
 		panic(err)
 	}
-	req.SetBasicAuth("admin", "geoserver")
+	req.SetBasicAuth(geoserver.Username, geoserver.Password)
 	req.Header.Add("Content-Type", "text/xml; charset=utf-8")
 	resp, err := client.Do(req)
 	if err != nil {
@@ -57,30 +109,30 @@ func datastoreName(filename string) string {
 }
 func uploadShapeFile(fileURI string) {
 	filename := filepath.Base(fileURI)
-	targetURL := fmt.Sprintf("%srest/workspaces/%s/datastores/%s/file.shp", geoserverURL, workspace, datastoreName(filename))
+	targetURL := fmt.Sprintf("%srest/workspaces/%s/datastores/%s/file.shp", geoserver.ServerUrl, geoserver.WorkspaceName, datastoreName(filename))
 	shapeFileBinary, err := ioutil.ReadFile(fileURI)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	_, workspaceErr := createWorkspace(workspace)
+	_, workspaceErr := createWorkspace(geoserver.WorkspaceName)
 	if workspaceErr != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	req, err := http.NewRequest("PUT", targetURL, bytes.NewBuffer(shapeFileBinary))
-	req.SetBasicAuth("admin", "geoserver")
+	req.SetBasicAuth(geoserver.Username, geoserver.Password)
 	req.Header.Set("Content-Type", "application/zip")
 	req.Header.Set("Accept", "application/xml")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	} else {
 		defer resp.Body.Close()
 
 		fmt.Println("response Status:", resp.Status)
 		responseCode, err := strconv.ParseInt(strings.TrimSpace(resp.Status), 10, 64)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 		if responseCode == 201 {
 			fmt.Println("Layer Uploaded Successfully")
