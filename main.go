db4ba27448224e3fc2adc8ea1ad21a484212df6e
hisham waleed karam <hisham.karam@cartologic.com>
2018-03-29 00:16:16 +0200

    fix package import

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"path/filepath"
 
 	"github.com/gorilla/mux"
-	"github.com/hishamkaram/uploadShapeFile/geoserver"
+	"github.com/hishamkaram/gsbinding/geoserver"
 )
 
 var uploadedPath = "./uploaded/"
