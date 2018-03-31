0a8bc50c320939e8e1f698635986169fb9b9ec3f
hisham waleed karam <hisham.karam@cartologic.com>
2018-03-31 15:41:29 +0200

    add 500 error code

--- vars.go
+++ vars.go
@@ -3,26 +3,28 @@ package geoserver
 import "errors"
 
 const (
-	statusOk           = 200
-	statusCreated      = 201
-	statusNotAllowed   = 405
-	statusNotFound     = 404
-	statusUnauthorized = 401
-	jsonType           = "application/json"
-	zipType            = "application/zip"
-	appXMLType         = "application/xml"
-	xmlType            = "text/xml"
-	sldType            = "application/vnd.ogc.sld+xml"
-	contentTypeHeader  = "Content-Type"
-	acceptHeader       = "Accept"
-	getMethod          = "GET"
-	putMethod          = "PUT"
-	postMethod         = "POST"
-	deleteMethod       = "DELETE"
+	statusOk            = 200
+	statusCreated       = 201
+	statusNotAllowed    = 405
+	statusInternalError = 500
+	statusNotFound      = 404
+	statusUnauthorized  = 401
+	jsonType            = "application/json"
+	zipType             = "application/zip"
+	appXMLType          = "application/xml"
+	xmlType             = "text/xml"
+	sldType             = "application/vnd.ogc.sld+xml"
+	contentTypeHeader   = "Content-Type"
+	acceptHeader        = "Accept"
+	getMethod           = "GET"
+	putMethod           = "PUT"
+	postMethod          = "POST"
+	deleteMethod        = "DELETE"
 )
 
 var statusErrorMapping = map[int]error{
-	statusNotAllowed:   errors.New("Method Not Allowed"),
-	statusNotFound:     errors.New("Not Found"),
-	statusUnauthorized: errors.New("Unauthorized"),
+	statusNotAllowed:    errors.New("Method Not Allowed"),
+	statusNotFound:      errors.New("Not Found"),
+	statusUnauthorized:  errors.New("Unauthorized"),
+	statusInternalError: errors.New("Internal Server Error"),
 }
