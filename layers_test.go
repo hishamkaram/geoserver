cff8029e660e8b63fa299ab6dee3975a404ff16f
hisham waleed karam <hisham.karam@cartologic.com>
2018-04-03 11:54:05 +0200

    add get layers test

--- layers_test.go
+++ layers_test.go
@@ -11,3 +11,9 @@ func TestGetshpFiledsName(t *testing.T) {
 	storename := gsCatalog.GetshpFiledsName("hisham.zip")
 	assert.Equal(t, storename, "hisham")
 }
+func TestGetLayers(t *testing.T) {
+	gsCatalog := GetCatalog("http://localhost:8080/geoserver/", "admin", "geoserver")
+	layers, err := gsCatalog.GetLayers("nurc")
+	assert.NotNil(t, layers)
+	assert.Nil(t, err)
+}
