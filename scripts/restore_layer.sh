58d1fa0c7e152343f44b5ec218af06e943c36226
hisham waleed karam <hisham.karam@cartologic.com>
2018-07-28 04:21:28 +0200

    fix wait for timeout

--- scripts/restore_layer.sh
+++ scripts/restore_layer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PGPASSWORD=golang
-./wait.sh -h localhost -p 5432 -t 0 -- pg_restore -d cartoview_datastore ../testdata/lbldyt
+./wait.sh -h localhost -p 5432 -t 600 -- pg_restore -d cartoview_datastore ../testdata/lbldyt
