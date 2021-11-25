4b0ca717b800485d79124ee45363f17453e4a141
mandalorian-one <mandalorian-one@users.noreply.github.com>
2021-11-25 14:04:37 +0500

    datastores tests refactored to be more stable

--- datastores_test.go
+++ datastores_test.go
@@ -2,6 +2,7 @@ package geoserver
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,18 +11,18 @@ import (
 
 type GeoserverDatastoreSuite struct {
 	suite.Suite
-	gsCatalog *GeoServer
+	gsCatalog     *GeoServer
+	conn          DatastoreConnection
+	workspaceName string
+	datastoreName string
 }
 
 func (suite *GeoserverDatastoreSuite) SetupSuite() {
-	suite.gsCatalog = GetCatalog("http://localhost:8080/geoserver/", "admin", "geoserver")
-	created, err := suite.gsCatalog.CreateWorkspace("datastores_test")
-	assert.True(suite.T(), created)
-	assert.Nil(suite.T(), err)
-}
-func TestGetDatastoreObj(t *testing.T) {
-	conn := DatastoreConnection{
-		Name:   "datastores_test",
+
+	suite.workspaceName = "datastores_test"
+	suite.datastoreName = "datastores_test"
+	suite.conn = DatastoreConnection{
+		Name:   suite.datastoreName,
 		Port:   5432,
 		Host:   "localhost",
 		Type:   "postgis",
@@ -29,78 +30,89 @@ func TestGetDatastoreObj(t *testing.T) {
 		DBPass: "xxxx",
 		DBUser: "postgres",
 	}
-	assert.NotNil(t, conn.GetDatastoreObj())
-}
-func (suite *GeoserverDatastoreSuite) TestCreateDatastore() {
-	conn := DatastoreConnection{
-		Name:   "datastores_test",
-		Port:   5432,
-		Type:   "postgis",
-		DBName: "cartoview_datastore",
-		DBPass: "xxxx",
-		DBUser: "postgres",
+
+	suite.gsCatalog = GetCatalog("http://localhost:8080/geoserver/", "admin", "geoserver")
+	created, err := suite.gsCatalog.CreateWorkspace(suite.workspaceName)
+	if err != nil && strings.Contains(err.Error(), "already exists") {
+		_, err = suite.gsCatalog.DeleteWorkspace(suite.workspaceName, true)
+		if err != nil {
+			assert.Fail(suite.T(), "can't setup suite")
+		}
+		created, err = suite.gsCatalog.CreateWorkspace(suite.workspaceName)
 	}
-	created, err := suite.gsCatalog.CreateDatastore(conn, "datastores_test")
 	assert.True(suite.T(), created)
 	assert.Nil(suite.T(), err)
-	created, err = suite.gsCatalog.CreateDatastore(conn, "datastores_test")
+}
+
+func (suite *GeoserverDatastoreSuite) TearDownSuite() {
+	deleted, err := suite.gsCatalog.DeleteWorkspace(suite.workspaceName, true)
+	assert.True(suite.T(), deleted)
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *GeoserverDatastoreSuite) CreateDatastore() (bool, error) {
+	return suite.gsCatalog.CreateDatastore(suite.conn, suite.workspaceName)
+}
+
+func (suite *GeoserverDatastoreSuite) Test01GetDatastoreObj() {
+	assert.NotNil(suite.T(), suite.conn.GetDatastoreObj())
+}
+
+func (suite *GeoserverDatastoreSuite) Test02CreateDatastore() {
+	created, err := suite.CreateDatastore()
+	assert.True(suite.T(), created)
+	assert.Nil(suite.T(), err)
+	created, err = suite.CreateDatastore()
 	assert.False(suite.T(), created)
 	assert.NotNil(suite.T(), err)
 }
 
-func (suite *GeoserverDatastoreSuite) TestDatastoreExists() {
-	exists, err := suite.gsCatalog.DatastoreExists("datastores_test", "datastores_test", true)
+func (suite *GeoserverDatastoreSuite) Test03DatastoreExists() {
+	exists, err := suite.gsCatalog.DatastoreExists(suite.workspaceName, suite.datastoreName, true)
 	assert.True(suite.T(), exists)
 	assert.Nil(suite.T(), err)
-	exists, err = suite.gsCatalog.DatastoreExists("datastores_test_dummy", "datastores_test_dummy", true)
+	exists, err = suite.gsCatalog.DatastoreExists(suite.workspaceName+"_dummy", suite.datastoreName+"_dummy", true)
 	assert.False(suite.T(), exists)
 	assert.NotNil(suite.T(), err)
 }
 
-func (suite *GeoserverDatastoreSuite) TestDeleteDatastore() {
-	deleted, err := suite.gsCatalog.DeleteDatastore("datastores_test", "datastores_test", true)
+func (suite *GeoserverDatastoreSuite) Test04DeleteDatastore() {
+	deleted, err := suite.gsCatalog.DeleteDatastore(suite.workspaceName, suite.datastoreName, true)
 	assert.True(suite.T(), deleted)
 	assert.Nil(suite.T(), err)
-	deleted, err = suite.gsCatalog.DeleteDatastore("datastores_test_dummy", "datastores_test_dummy", true)
+	deleted, err = suite.gsCatalog.DeleteDatastore(suite.workspaceName+"_dummy", suite.datastoreName+"_dummy", true)
 	assert.False(suite.T(), deleted)
 	assert.NotNil(suite.T(), err)
 }
-func (suite *GeoserverDatastoreSuite) TestGetDatastores() {
-	datastores, err := suite.gsCatalog.GetDatastores("topp")
+
+func (suite *GeoserverDatastoreSuite) Test05GetDatastores() {
+	_, _ = suite.CreateDatastore()
+	datastores, err := suite.gsCatalog.GetDatastores(suite.workspaceName)
 	assert.NotEmpty(suite.T(), datastores)
 	assert.NotNil(suite.T(), datastores)
 	assert.Nil(suite.T(), err)
-	datastores, err = suite.gsCatalog.GetDatastores("topp_dummy")
+	datastores, err = suite.gsCatalog.GetDatastores(suite.workspaceName + "_wrong")
 	assert.Nil(suite.T(), datastores)
 	assert.NotNil(suite.T(), err)
 }
-func (suite *GeoserverDatastoreSuite) TestGetDatastoreDetails() {
-	datastore, err := suite.gsCatalog.GetDatastoreDetails("sf", "sf")
+
+func (suite *GeoserverDatastoreSuite) Test06GetDatastoreDetails() {
+	datastore, err := suite.gsCatalog.GetDatastoreDetails(suite.workspaceName, suite.datastoreName)
 	assert.NotEmpty(suite.T(), datastore)
 	assert.NotNil(suite.T(), datastore)
 	assert.Nil(suite.T(), err)
-	datastore, err = suite.gsCatalog.GetDatastoreDetails("sf", "sf_dummy")
+	datastore, err = suite.gsCatalog.GetDatastoreDetails(suite.workspaceName, suite.datastoreName+"_dummy")
 	assert.Equal(suite.T(), datastore, &Datastore{})
 	assert.NotNil(suite.T(), err)
 }
 
-func (suite *GeoserverDatastoreSuite) TearDownSuite() {
-	deleted, err := suite.gsCatalog.DeleteWorkspace("datastores_test", true)
-	assert.True(suite.T(), deleted)
-	assert.Nil(suite.T(), err)
-}
 func TestGeoserverDatastoreSuite(t *testing.T) {
 	suite.Run(t, new(GeoserverDatastoreSuite))
 }
-func TestGeoserverImplemetDatastoreService(t *testing.T) {
+
+func TestGeoserverImplementDatastoreService(t *testing.T) {
 	gsCatalog := reflect.TypeOf(&GeoServer{})
 	DatastoreServiceType := reflect.TypeOf((*DatastoreService)(nil)).Elem()
 	check := gsCatalog.Implements(DatastoreServiceType)
-	assert.True(t, check)
-}
-func TestGeoserverImplementDatastoreService(t *testing.T) {
-	gsCatalog := reflect.TypeOf(&GeoServer{})
-	CatalogType := reflect.TypeOf((*DatastoreService)(nil)).Elem()
-	check := gsCatalog.Implements(CatalogType)
 	assert.True(t, check)
 }
